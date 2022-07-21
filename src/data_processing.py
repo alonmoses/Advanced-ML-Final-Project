9b7249eaf2cb1376505c42cbfe6057522ec7105b
Alon Moses <alon2332@gmail.com>
2022-07-21 07:30:47 +0300

    adding basic feature selection- need to review and fix

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class ProcessData:
     def __init__(self, train_data, test_data):
@@ -6,16 +7,29 @@ class ProcessData:
         self.test_data = test_data
 
     def load_data_to_dataframe(self):
-        chosen_columns = ['text', 'subreddit', 'social_timestamp', 'confidence', 'label'] ## Need to choose columns in more sophosticated way, as presented in the paper
-        train_df = pd.read_csv(self.train_data, usecols=chosen_columns)
-        test_df = pd.read_csv(self.test_data, usecols=chosen_columns)
+        # load csv to dataframe
+        train_df = pd.read_csv(self.train_data)
+        test_df = pd.read_csv(self.test_data)
+
+        # drop columns based on remove_features() logic
+        drop_columns = self.remove_features(train_df)
+        print(drop_columns)
+        train_df.drop(drop_columns, axis=1, inplace=True)
+        test_df.drop(drop_columns, axis=1, inplace=True)
+
         return train_df, test_df
+
+    def remove_features(self, df, rho=0.4):
+        corr_matrix = df.corr().abs() # correlation matrix
+        upper_corr_matrix = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool)) # upper triangle of matrix
+        features_to_drop = [column for column in upper_corr_matrix.columns if any(upper_corr_matrix[column] > rho)] # get relevant features- TODO: NEED TO FIX
+        return features_to_drop
 
 
 def main():
-    dataset = ProcessData("/aml/data/dreaddit/dreaddit-train.csv", "../data/dreaddit-test.csv")
+    dataset = ProcessData("/aml/data/dreaddit/dreaddit-train.csv", "/aml/data/dreaddit/dreaddit-test.csv")
     train_df, test_df = dataset.load_data_to_dataframe()
-
+    print(train_df.head())
 
 if __name__ == '__main__':
     main()
